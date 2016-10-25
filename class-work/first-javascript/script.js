864da812e9f96792f941885f5ce845e270bcadd3
ashley <ashleykapturkiewicz@gmail.com>
2016-10-24 21:00:10 -0400

    Classwork 10-24-16

---
+++ class-work/first-javascript/script.js
@@ -0,0 +1,37 @@
+//alert('this is an alert! this is annoying!!');
+//document.write('heyyyyyyyyy');
+
+
+// document.write('<p>' + 10 * 100 + '</p>');
+// document.write('<p>' + 100 / 10 + '</p>');
+// document.write('<p>' + (50 - 17) + '</p>');
+// document.write('<p>' + (50 + 17) + '</p>');
+
+// var firstName;
+// var lastName;
+
+// firstName = 'Ashley';
+// lastName = 'K';
+
+// document.write(`<p>Your name is ${firstName} ${lastName}.</p>`);
+
+// var num1 = 10 * 10;
+
+// document.write(num1);
+
+
+
+// function hello () {
+//   var greeting = 'hello!';
+
+//   greeting = 'hello, world!';
+
+//   alert(greeting);
+// }
+
+
+// hello();
+
+document.getElementById('title').style.color = 'red';
+
+document.querySelector('#text-chunk').style.color = 'green';
